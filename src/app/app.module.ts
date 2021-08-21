ee7fc6ad394e35a6dd451bcb14806d8c38058a44
Esther-Moki <esther.moki@student.moringaschool.com>
2021-08-21 13:00:10 +0300

    generated a quote component form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './components/quote/quote.component';
+import { QuoteDetailComponent } from './components/quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
